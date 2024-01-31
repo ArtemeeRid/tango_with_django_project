658fdce523c53d786a5a76c9363252ed16c95c68
Ameer Azlan Shah <2774679a@student.gla.ac.uk>
2024-01-31 16:06:06 +0000

    chapter 4 finished, but the pictures aren't exactly what the book wants

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [    
     path('', views.index, name='index'),
@@ -24,3 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+# Serving the media files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += staticfiles_urlpatterns()
